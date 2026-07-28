229bd57b97540d3b7231f76e828f94639e3812dc
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-07-29 00:15:49 +0300

    Fixed download issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ from mercator2equirectangular import mercator_to_equirectangular
 
 st.title("Convert Mercator Projection Maps to Equirectangular Projection")
 
-st.write('For best results, use the so-called "Web Mercator" or "Google Mercator" projection, which terminates the map at approximately ±89.05°.')
+st.write('For best results, use the so-called "Web Mercator" (also called the "Google Mercator" or "Pseudo-Mercator") projection, which projects onto a sphere instead of an ellipsoid and terminates the map at approximately ±85.05°.')
 st.write("If your Mercator map is approximately square, chances are you're using this projection!")
 
-uploaded_file = st.file_uploader("Upload your Mercator map here...")
+uploaded_file = st.file_uploader("Upload your Mercator map here...", accept_multiple_files=False, max_upload_size=1000)
 
 
 if uploaded_file is not None:
@@ -22,10 +22,16 @@ if uploaded_file is not None:
         st.write("You uploaded this Mercator map:")
         st.image(f"./map.{extension}")
 
-        mercator_to_equirectangular(f"./map.{extension}", f"converted_map.{extension}")
+        output_path = "converted_map.jpg"
+        mercator_to_equirectangular(f"./map.{extension}", output_path)
 
         st.write("Here is your converted Equirectangular map:")
-        st.image(f"converted_map.{extension}")
+        st.image(output_path)
+
+        with open(output_path, "rb") as f:
+            image_bytes = f.read()
+
+        st.download_button("Download", image_bytes, file_name=output_path, mime="image/jpeg")
 
         st.write("Have a nice day!")
 
